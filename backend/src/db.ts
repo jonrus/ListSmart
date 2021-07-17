753167d7e7e67a3d0cf32940faa574e286031430
Jon Russell <jon@jonrussell.xyz>
2021-07-17 14:48:07 -0500

    Backend 	Can now add an item to a List 	Can now delete an item from a List

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,7 +2,7 @@ import {MongoClient, ObjectId} from "mongodb";
 import {DB_URI} from "./config";
 
 export interface ListItem {
-    id: string,
+    id?: string,
     title: string,
     quanity?: number,
     complete: boolean,
@@ -38,7 +38,7 @@ class ListSmartMongoDBHelper {
         }
         catch {
             console.log("Unable to connect to DB");
-            return ({sucess: false, msg: "Unable to connec to DB"});
+            return ({sucess: false, msg: "Unable to connect to DB"});
         }
         finally {
             this._client.close();
@@ -62,7 +62,55 @@ class ListSmartMongoDBHelper {
         }
         catch {
             console.log("Unable to connect to DB");
-            return ({sucess: false, msg: "Unable to connec to DB"});
+            return ({sucess: false, msg: "Unable to connect to DB"});
+        }
+        finally {
+            this._client.close();
+        }
+    }
+    
+    async newItemInList(mainListID: string, listID: string, itemName: string) {
+        try {
+            await this._client.connect();
+
+            //Build the Mondgo doc
+            const filter = {_id: new ObjectId(mainListID), "lists.id": new ObjectId(listID)};
+            const newItemID = new ObjectId();
+            //TODO spread item data from API route into the doc
+            const newDoc = {$push: {"lists.$.items": {id: newItemID, title: itemName}}};
+
+            const res = await this._client.db("ListSmart").collection("ListOfLists").updateOne(
+                filter, newDoc);
+
+            if (res.matchedCount === 1) return ({sucess: true, id: newItemID});
+            return ({sucess: false});
+        }
+        catch {
+            console.log("Unable to connect to DB");
+            return ({sucess: false, msg: "Unable to connect to DB"});
+        }
+        finally {
+            this._client.close();
+        }
+    }
+
+    async deleteItemInList(mainListID: string, listID: string, itemID: string) {
+        try {
+            await this._client.connect();
+
+            //Build the Mondgo doc
+            const filter = {_id: new ObjectId(mainListID), "lists.id": new ObjectId(listID), "lists.items.id": new ObjectId(itemID)};
+            const newDoc = {$pull: {"lists.$.items": {id: new ObjectId(itemID)}}};
+
+            const res = await this._client.db("ListSmart").collection("ListOfLists").updateOne(
+                filter, newDoc);
+
+            if (res.matchedCount === 1) return ({sucess: true, id: itemID});
+            return ({sucess: false});
+        }
+        catch {
+            console.log("Unable to connect to DB");
+            return ({sucess: false, msg: "Unable to connect to DB"});
         }
         finally {
             this._client.close();
--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -20,11 +20,33 @@ APIRouter.put("/new/master-list", (async (req, res) => {
     return res.status(500).json({error: true, msg: dbRes});
 }));
 
+//New list in list
 APIRouter.put("/new/sub-list", (async (req, res) => {
     const {id, title} = req.body;
-    if (!id || !title) return res.status(500).json({error: true, msg: "Master list id and new list title are required"});
+    if (!id || !title) return res.status(500).json({error: true, msg: "Main list id and new list title are required"});
     
     const dbRes = await db.newListInList(id, title);
+    if (dbRes?.sucess) return res.json({error: false, id: dbRes?.id});
+    return res.status(500).json({error: true, msg: dbRes});
+}));
+
+//New item in list
+APIRouter.put("/new/item", (async (req, res) => {
+    //TODO construct a new item object to pass to db helper class
+    const {mainID, listID, itemName} = req.body;
+    if (!mainID || !listID || !itemName) return res.status(500).json({error: true, msg: "Main list id, list id and new item name are required"});
+    
+    const dbRes = await db.newItemInList(mainID, listID, itemName);
+    if (dbRes?.sucess) return res.json({error: false, id: dbRes?.id});
+    return res.status(500).json({error: true, msg: dbRes});
+}));
+
+//Delete item (with id) from a list
+APIRouter.delete("/del/item", (async (req, res) => {
+    const {mainID, listID, itemID} = req.body;
+    if (!mainID || !listID || !itemID) return res.status(500).json({error: true, msg: "Main list id, list id and item id are required"});
+    
+    const dbRes = await db.deleteItemInList(mainID, listID, itemID);
     if (dbRes?.sucess) return res.json({error: false, id: dbRes?.id});
     return res.status(500).json({error: true, msg: dbRes});
 }));
