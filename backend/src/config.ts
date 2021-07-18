0d589142c1b6c6e771407fad7a6188e3e1e9ad17
Jon Russell <jon@jonrussell.xyz>
2021-07-18 11:33:18 -0500

    Backend 	Updated DB class to use env vars for DB name and collection 	Updated example env file to reflect class changes.

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,3 +4,5 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 export const DB_URI = process.env.MONGO_URI;
+export const DB_NAME = process.env.MONGO_DB_NAME || "ListSmart";
+export const DB_COLLECTION = process.env.MONGO_DB_COLLECTION || "ListOfLists";
--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,5 +1,5 @@
 import {MongoClient, ObjectId} from "mongodb";
-import {DB_URI} from "./config";
+import {DB_URI, DB_NAME, DB_COLLECTION} from "./config";
 
 export interface ListItem {
     id?: string,
@@ -32,7 +32,7 @@ class ListSmartMongoDBHelper {
 
             //Build the Mondgo doc
             const newListDoc = {lists: []}
-            const res = await this._client.db("ListSmart").collection("ListOfLists").insertOne(newListDoc);
+            const res = await this._client.db(DB_NAME).collection(DB_COLLECTION).insertOne(newListDoc);
 
             if (res.insertedId) return ({sucess: true, id: res.insertedId});
             return ({sucess: false, msg: "Unable to insert into collection"});
@@ -54,7 +54,7 @@ class ListSmartMongoDBHelper {
             const newListID = new ObjectId();
             const newDoc = {$push: {lists: {id: newListID, title: listName, items: []}}};
 
-            const res = await this._client.db("ListSmart").collection("ListOfLists").updateOne(
+            const res = await this._client.db(DB_NAME).collection(DB_COLLECTION).updateOne(
                 filter, newDoc);
 
             if (res.matchedCount === 1) return ({sucess: true, id: newListID});
@@ -78,7 +78,7 @@ class ListSmartMongoDBHelper {
             //TODO spread item data from API route into the doc
             const newDoc = {$push: {"lists.$.items": {id: newItemID, title: itemName}}};
 
-            const res = await this._client.db("ListSmart").collection("ListOfLists").updateOne(
+            const res = await this._client.db(DB_NAME).collection(DB_COLLECTION).updateOne(
                 filter, newDoc);
 
             if (res.matchedCount === 1) return ({sucess: true, id: newItemID});
@@ -100,7 +100,7 @@ class ListSmartMongoDBHelper {
             const filter = {_id: new ObjectId(mainListID), "lists.id": new ObjectId(listID), "lists.items.id": new ObjectId(itemID)};
             const newDoc = {$pull: {"lists.$.items": {id: new ObjectId(itemID)}}};
 
-            const res = await this._client.db("ListSmart").collection("ListOfLists").updateOne(
+            const res = await this._client.db(DB_NAME).collection(DB_COLLECTION).updateOne(
                 filter, newDoc);
 
             if (res.matchedCount === 1) return ({sucess: true, id: itemID});
@@ -124,7 +124,7 @@ class ListSmartMongoDBHelper {
             const filter = {_id: new ObjectId(mainListID)};
             const newDoc = {$pull: {lists: {id: new ObjectId(listID)}}};
 
-            const res = await this._client.db("ListSmart").collection("ListOfLists").updateOne(
+            const res = await this._client.db(DB_NAME).collection(DB_COLLECTION).updateOne(
                 filter, newDoc);
 
             if (res.modifiedCount === 1) return ({sucess: true, id: listID});
@@ -147,7 +147,7 @@ class ListSmartMongoDBHelper {
 
             const filter = {_id: new ObjectId(mainListID)};
 
-            const res = await this._client.db("ListSmart").collection("ListOfLists").deleteOne(filter);
+            const res = await this._client.db(DB_NAME).collection(DB_COLLECTION).deleteOne(filter);
 
             if (res.deletedCount === 1) return ({sucess: true, id: mainListID});
             return ({sucess: false, msg: "No mathching list"});
@@ -166,7 +166,7 @@ class ListSmartMongoDBHelper {
             await this._client.connect();
 
             const filter = {_id: new ObjectId(mainListID)};
-            const res = await this._client.db("ListSmart").collection("ListOfLists").findOne(filter);
+            const res = await this._client.db(DB_NAME).collection(DB_COLLECTION).findOne(filter);
 
             if (res) return ({sucess: true, id: mainListID, data: res});
             return ({sucess: false, msg: "No mathching list"});
